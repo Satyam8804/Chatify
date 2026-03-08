7fa933767dc297356e73b3b385c18578c4ad07ef
Satyam Kumar <satyam8804378323@gmail.com>
2026-03-08 23:34:57 +0530

    updated backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://chatify-jux9.onrender.com",
+  baseURL: "https://chatify-jux9.onrender.com/api",
   withCredentials: true, // IMPORTANT for refresh token cookie
 });
 
