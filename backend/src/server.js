d472920cfb62ad039ed1b5d16f40e49282bd8c56
Satyam Kumar <satyam8804378323@gmail.com>
2026-04-08 22:38:54 +0530

    added admin panel

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,13 +36,6 @@ app.use(
   })
 );
 
-// ✅ force HTTPS
-app.use((req, res, next) => {
-  if (req.headers["x-forwarded-proto"] !== "https") {
-    return res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-  next();
-});
 
 
 app.use(express.json());
